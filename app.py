ae8ef41a634150e69faf5a4dd3009fb46390173e
Niufanfanfan <139232046+Niufanfanfan@users.noreply.github.com>
2023-07-12 10:31:42 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+@app.route('/about.html')
+def about():  # put application's code here
+    return render_template('about.html')
+
+@app.route('/index.html')
+def index1():  # put application's code here
+    return render_template('index.html')
+
+@app.route ('/index')
+def index():  # put application's code here
+    return render_template('index.html')
+
+@app.route('/blog.html')
+def blog():  # put application's code here
+    return render_template('blog.html')
+
+@app.route('/services.html')
+def services():  # put application's code here
+    return render_template('services.html')
+
+@app.route('/blogsingle.html')
+def blogsingle():  # put application's code here
+    return render_template('blogsingle.html')
+
+if __name__ == '__main__':
+    app.run()
